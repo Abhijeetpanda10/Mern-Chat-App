5e83707bbb9a4aa55538586125e9699eca10d2b8
Abhijeetpanda10 <133777997+Abhijeetpanda10@users.noreply.github.com>
2025-10-28 22:34:10 +0530

    Fix case sensitivity in import paths
    Corrected the case of 'Controllers' to 'controllers' in import statements.

--- backend/Routes/userRoutes.js
+++ backend/Routes/userRoutes.js
@@ -4,11 +4,11 @@ const fetchuser = require("../middleware/fetchUser.js");
 const {
   getPresignedUrl,
   getOnlineStatus,
-} = require("../Controllers/userController.js");
+} = require("../controllers/userController.js");  // 👈 lowercase "controllers"
 const {
   getNonFriendsList,
   updateprofile,
-} = require("../Controllers/auth_controller.js");
+} = require("../controllers/auth_controller.js");  // 👈 lowercase "controllers"
 
 router.get("/online-status/:id", fetchuser, getOnlineStatus);
 router.get("/non-friends", fetchuser, getNonFriendsList);
